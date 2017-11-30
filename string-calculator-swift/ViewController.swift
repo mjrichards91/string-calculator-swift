ddd116dda0ed17c77dbe425b06fa7665db7fc540
Mike Richards <mjrichards91@gmail.com>
2017-11-30 08:52:00 -0500

    Initial Commit

---
+++ string-calculator-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  string-calculator-swift
+//
+//  Created by Michael Richards on 11/30/17.
+//  Copyright © 2017 Michael Richards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
